55b1e0c47ce2588b32d766c5e98f0f4431d61490
MOLEHOUR <79060569+MOLEHOUR@users.noreply.github.com>
2024-04-13 13:21:16 -0500

    dada

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -13,7 +13,7 @@ func _physics_process(_delta):
 	var current_location = global_transform.origin
 	var next_location = nav_agent.get_next_path_position()
 	new_velocity = (next_location - current_location).normalized() * SPEED
-	if _player.flashlight.on == true:
+	if _player.flashlight.on == true and _flashlightArea == true:
 		nav_agent.target_position = _nest.global_transform.origin
 	else:
 		nav_agent.target_position = _player.global_transform.origin
@@ -25,6 +25,9 @@ func _physics_process(_delta):
 		var collision = get_slide_collision(i)
 		if collision.get_collider().name == "Player":
 			get_tree().change_scene_to_file("res://scenes/cole_test_scene.tscn")
+	for i in _player.flashlight_area.is_colliding():
+		if i == self:
+			_flashlightArea = true
 #gets target location	
 func update_target_location(player, nest):
 	_player = player
