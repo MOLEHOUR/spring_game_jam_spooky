159007245019e4d3dca94fabc98c665d0a891f5b
MOLEHOUR <coleblue01@gmail.com>
2024-04-13 19:37:39 -0500

    beta

--- scripts/main_win.gd
+++ scripts/main_win.gd
@@ -6,11 +6,11 @@ func _ready():
 	focus()
 
 func _on_restart_button_down():
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://scenes/mainLevel.tscn")
 
 func _on_quit_button_down():
 	#Go to main menu
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn") #repalce with main menu
+	get_tree().change_scene_to_file("res://scenes/cole_test_scene.tscn") #repalce with main menu
 
 func focus():
 	restart.grab_focus()
